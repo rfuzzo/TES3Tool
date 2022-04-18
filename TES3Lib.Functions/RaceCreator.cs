cd230df24300e85af6c2b2291d44f7c623b08d04
rfuzzo <r.fuzzo@gmail.com>
2022-04-18 14:37:52 +0200

    add Tes3Lib.sln

--- TES3Lib.Functions/RaceCreator.cs
+++ TES3Lib.Functions/RaceCreator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TES3Lib.Base;
 using TES3Lib.Enums;
 using TES3Lib.Enums.Flags;
@@ -13,7 +12,7 @@ namespace TES3Lib.Functions
     /// </summary>
     public static class RaceCreator
     {
-        private static HashSet<BodyPart> bodyParts = new HashSet<BodyPart>
+        private static readonly HashSet<BodyPart> bodyParts = new HashSet<BodyPart>
         {
             BodyPart.Ankle, BodyPart.Chest,BodyPart.Foot, BodyPart.Forearm,
             BodyPart.Groin, BodyPart.Hand, BodyPart.Knee, BodyPart.Neck,
@@ -28,17 +27,21 @@ namespace TES3Lib.Functions
             if (!config.IsBodyPartsOnly)
             {
                 var race = new RACE();
-                race.NAME.EditorId = String.IsNullOrEmpty(config.EditorId) ? "\0" : $"{config.EditorId}\0";
-                race.FNAM.Name = String.IsNullOrEmpty(config.EditorId) ? "\0" : $"{config.Name}\0";
-                race.DESC.Description = String.IsNullOrEmpty(config.EditorId) ? "\0" : $"{config.Description}\0";
+                race.NAME.EditorId = string.IsNullOrEmpty(config.EditorId) ? "\0" : $"{config.EditorId}\0";
+                race.FNAM.FileName = string.IsNullOrEmpty(config.EditorId) ? "\0" : $"{config.Name}\0";
+                race.DESC.Description = string.IsNullOrEmpty(config.EditorId) ? "\0" : $"{config.Description}\0";
                 outputRecords.Add(race);
             }
 
-            if(config.IsMale)
+            if (config.IsMale)
+            {
                 outputRecords.AddRange(CreateBodyParts(config, true, bodyParts));
+            }
 
             if (config.IsFemale)
+            {
                 outputRecords.AddRange(CreateBodyParts(config, false, bodyParts));
+            }
 
             return outputRecords;
         }
@@ -53,7 +56,7 @@ namespace TES3Lib.Functions
             {
                 var body = new BODY();
                 body.BYDT.BodyPart = BodyPart.Hand;
-                body.FNAM.Name = config.EditorId;
+                body.FNAM.FileName = config.EditorId;
                 body.MODL.ModelPath = $"{config.ModelFolderPath}_{symbol}_body1st.nif\0";
                 body.BYDT.PartType = BodyPartType.Skin;
                 body.NAME.EditorId = $"{config.EditorIdPrefix}b_n_{EditorIdTrimmed}_{symbol}_hand.1st\0";
@@ -65,19 +68,19 @@ namespace TES3Lib.Functions
 
                 outputRecords.Add(body);
             }
-        
+
             foreach (var part in bodyParts)
             {
                 var body = new BODY();
                 body.BYDT.BodyPart = part;
-                body.FNAM.Name = config.EditorId;
-                
-                if(part.Equals(BodyPart.Head))
+                body.FNAM.FileName = config.EditorId;
+
+                if (part.Equals(BodyPart.Head))
                 {
                     body.NAME.EditorId = $"{config.EditorIdPrefix}b_n_{EditorIdTrimmed}_{symbol}_head01\0";
                     body.MODL.ModelPath = $"{config.ModelFolderPath}_{symbol}_head01.nif\0";
                 }
-                else if(part.Equals(BodyPart.Hair))
+                else if (part.Equals(BodyPart.Hair))
                 {
                     body.NAME.EditorId = $"{config.EditorIdPrefix}b_n_{EditorIdTrimmed}_{symbol}_hair01\0";
                     body.MODL.ModelPath = $"{config.ModelFolderPath}_{symbol}_hair01.nif\0";
@@ -89,7 +92,7 @@ namespace TES3Lib.Functions
                 }
 
                 body.BYDT.PartType = BodyPartType.Skin;
-               
+
                 body.BYDT.Flags.Add(BodyPartFlag.Playable);
                 if (!IsMale)
                 {
@@ -127,7 +130,7 @@ namespace TES3Lib.Functions
 
             public CreatorConfig()
             {
-                EditorIdPrefix = String.Empty;
+                EditorIdPrefix = string.Empty;
                 IsBodyPartsOnly = false;
                 IsMale = true;
                 IsFemale = true;
