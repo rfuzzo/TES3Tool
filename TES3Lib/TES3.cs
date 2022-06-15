2da4c56525cca4ba0d815b854bd053cad416badb
NullCascade <nullcascade@gmail.com>
2022-06-15 14:54:02 -0700

    Make async loading of records safer, as List is not thread-safe. This fixes conditions where records could be null.

--- TES3Lib/TES3.cs
+++ TES3Lib/TES3.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using TES3Lib.Base;
 using Utility;
@@ -11,6 +12,7 @@ namespace TES3Lib
     {
         private const int HeaderSize = 16;
         public List<Record> Records { get; set; } = new();
+        private Mutex RecordsMutex { get; } = new();
         public string Path { get; set; }
 
         public static TES3 TES3Load(string filePath, List<string> filteredGrops = null)
@@ -42,9 +44,9 @@ namespace TES3Lib
                 var data = new byte[HeaderSize + size];
                 fileStream.Read(data, 0, HeaderSize + size);
 
-                TES3.Records.Add(null);
+                TES3.AddRecordThreadSafe(null);
                 var index = TES3.Records.Count - 1;
-                tasks.Add(new Task(() => RecordBuildTask(name, data, TES3.Records, index)));
+                tasks.Add(new Task(() => TES3.SetRecordAtIndexThreadSafe(index, BuildRecord(name, data))));
                 tasks[index].Start();
             }
 
@@ -80,20 +82,17 @@ namespace TES3Lib
                 var data = new byte[HeaderSize + size];
                 fileStream.Read(data, 0, HeaderSize + size);
 
-                TES3.Records.Add(null);
-                var index = TES3.Records.Count - 1;
-                RecordBuildTask(name, data, TES3.Records, index);
+                TES3.Records.Add(BuildRecord(name, data));
             }
 
             return TES3;
         }
 
-        public static void RecordBuildTask(string name, byte[] data, List<Record> records, int index)
+        public static Record BuildRecord(string name, byte[] data)
         {
             var assembly = Assembly.GetExecutingAssembly();
-            var record = assembly
+            return assembly
                 .CreateInstance($"TES3Lib.Records.{name}", false, BindingFlags.Default, null, new object[] { data }, null, null) as Record;
-            records[index] = record;
         }
 
         public void TES3Save(string filePath)
@@ -105,6 +104,29 @@ namespace TES3Lib
 
                 fs.Write(serializedRecord, 0, serializedRecord.Length);
             }
+        }
+
+        /// <summary>
+        /// Adds a record to the end of the Records list. Makes use of an internal mutex to ensure that it is thread-safe.
+        /// </summary>
+        /// <param name="record">The record to append.</param>
+        public void AddRecordThreadSafe(Record record)
+        {
+            RecordsMutex.WaitOne();
+            Records.Add(record);
+            RecordsMutex.ReleaseMutex();
+        }
+
+        /// <summary>
+        /// Sets a record at a given index in the Records list. Makes use of an internal mutex to ensure that it is thread-safe.
+        /// </summary>
+        /// <param name="index">The index to set at.</param>
+        /// <param name="record">The record to set.</param>
+        public void SetRecordAtIndexThreadSafe(int index, Record record)
+        {
+            RecordsMutex.WaitOne();
+            Records[index] = record;
+            RecordsMutex.ReleaseMutex();
         }
     }
 }
