47bcd23b7d2e35178cb6a857e864b95601b64bd6
rfuzzo <r.fuzzo@gmail.com>
2023-07-06 08:33:12 +0200

    maui

---
+++ Tes3EditX/Tes3EditX.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TES3Lib;
+
+Console.WriteLine("Hello, World!");
+
+var testPlugin = "test.esp";
+if (Path.Exists(testPlugin))
+{
+    var bytes = File.ReadAllBytes(testPlugin);
+    var plugin = TES3.TES3Load(testPlugin);
+    
+    Console.WriteLine($"{testPlugin}");
+    foreach (var record in plugin.Records)   
+    {
+        Console.WriteLine($"\t{record.GetEditorId()}");
+    }
+}
+
+
---
+++ Tes3EditX/Tes3EditX.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tes3EditX.MAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
